10e790f7e2db64e618a6e7e038fe9f0b9b3dd346
anh-daongoc <anh.daongoc@comnosoft.com>
2025-10-08 16:21:20 +0700

    update tables

--- src/pages/Tables/DeviceTables.tsx
+++ src/pages/Tables/DeviceTables.tsx
@@ -1,20 +1,20 @@
 import PageBreadcrumb from "../../components/common/PageBreadCrumb";
 import ComponentCard from "../../components/common/ComponentCard";
 import PageMeta from "../../components/common/PageMeta";
-import DataTable from "../../components/tables/BasicTables/DataTable";
+import DeviceTable from "../../components/tables/DeviceTables";
 
-export default function BasicTables() {
+export default function UserTables() {
   return (
     <>
       <PageMeta
         title="React.js Basic Tables Dashboard | TailAdmin - Next.js Admin Dashboard Template"
         description="This is React.js Basic Tables Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
       />
-      <PageBreadcrumb pageTitle="User" />
+      <PageBreadcrumb pageTitle="Device" />
       <div className="space-y-6">
-        <ComponentCard title="User Table">
+        <ComponentCard title="Device Table">
           {/* <BasicTableOne/> */}
-          <DataTable/>
+          <DeviceTable/>
         </ComponentCard>
       </div>
     </>
---
+++ src/pages/Tables/FirmwareTables.tsx
@@ -0,0 +1,22 @@
+import PageBreadcrumb from "../../components/common/PageBreadCrumb";
+import ComponentCard from "../../components/common/ComponentCard";
+import PageMeta from "../../components/common/PageMeta";
+import FirmwareTable from "../../components/tables/FirmwareTables";
+
+export default function UserTables() {
+  return (
+    <>
+      <PageMeta
+        title="React.js Basic Tables Dashboard | TailAdmin - Next.js Admin Dashboard Template"
+        description="This is React.js Basic Tables Dashboard page for TailAdmin - React.js Tailwind CSS Admin Dashboard Template"
+      />
+      <PageBreadcrumb pageTitle="Firmware" />
+      <div className="space-y-6">
+        <ComponentCard title="Firmware Table">
+          {/* <BasicTableOne/> */}
+          <FirmwareTable/>
+        </ComponentCard>
+      </div>
+    </>
+  );
+}
